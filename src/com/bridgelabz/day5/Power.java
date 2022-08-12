e3db7a3d51f25f936b95dcf46c599f35f23ce505
saurabh jain <luckyjain0107@gmai.com>
2022-08-12 14:25:14 +0530

    Power

---
+++ src/com/bridgelabz/day5/Power.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.day5;
+
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args) {
+        int n, p;
+        int result = 1;
+        System.out.println("enter number");
+        Scanner r = new Scanner(System.in);
+        n = r.nextInt();
+        System.out.println("enter power");
+        p = r.nextInt();
+        for (int i = 1; i <= p; i++)
+        {
+            result = n * result;
+        }
+        System.out.println("power "+result);
+    }
+}
